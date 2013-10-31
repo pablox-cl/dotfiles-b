1b1057ae1ce227ff70ddacbe1e8bf5f558a0bcd1
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2013-10-31 17:58:33 -0300

    Add support for a 'vendor everything' approach in ruby development

---
+++ zsh.d/after/path.zsh
@@ -0,0 +1,7 @@
+# Make sure *vendored* gems are run first than anything (useful with rbenv)
+# See:
+#   + http://jimeh.me/blog/2011/11/01/my-ruby-development-environment/
+#   + http://ryan.mcgeary.org/2011/02/09/vendor-everything-still-applies/
+
+export PATH="./vendor/bundle/bin:$PATH"
+
