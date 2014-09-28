8aaa50902d2f50d46249642ee960aee2707db030
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2014-09-28 16:03:18 -0300

    [zsh] remove HIST_FCNTL_LOCK zsh option

--- config/zsh/settings/zshsetopts.zsh
+++ config/zsh/settings/zshsetopts.zsh
@@ -4,7 +4,6 @@
 setopt INTERACTIVE_COMMENTS      # allow the use of `# ` in the command line
 
 setopt APPEND_HISTORY            # allow multiple terminal sessions to append
-setopt HIST_FCNTL_LOCK           # better performance on history file
 setopt HIST_REDUCE_BLANKS        # remove extra blanks for each command line
 unsetopt LIST_BEEP               # no more beeps
 
