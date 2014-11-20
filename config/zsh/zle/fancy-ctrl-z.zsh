baad748e2eb75b35463597729255cba6e5ff2db1
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2014-11-19 23:35:52 -0300

    [zsh] Lighting fast switch between console apps with Ctrl-z

---
+++ config/zsh/zle/fancy-ctrl-z.zsh
@@ -0,0 +1,14 @@
+# Take from:
+# http://sheerun.net/2014/03/21/how-to-boost-your-vim-productivity/
+
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
